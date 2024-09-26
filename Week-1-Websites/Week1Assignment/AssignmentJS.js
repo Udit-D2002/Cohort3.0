6da16455b07d2f9f65647a0651655b5c9f57fa3d
Udit D <dudit3128@gmail.com>
2024-09-26 17:19:55 +0530

    Learning from Cohort 3.0

---
+++ Week-1-Websites/Week1Assignment/AssignmentJS.js
@@ -0,0 +1,33 @@
+// Assignmetnt 1
+let favourite_colour = "White";
+console.log(favourite_colour);
+
+const height = 176;
+console.log(height);
+
+let like_pizza = true;
+console.log(like_pizza);
+// Assignment 1 complete. (from Website :https://projects.100xdevs.com/tracks/javascript-1/Javascript-101-4)
+
+
+// Testing == operator in JS
+let equalityAssociated = 1;
+let equalityChecked = 1;
+if (equalityAssociated == equalityChecked) {
+    console.log("equalityAssociated and equalityChecked are equal and correct, if condition syntax.");
+}
+// How == works : Double equals (==) will try to convert the values to the same data type and then try to compare them. 
+// Testing == operator complete
+
+
+
+// Testing === operator in JS
+
+let tripleEquality = 1;
+let tripleEqualityCheck = 1;
+if (tripleEquality === tripleEqualityCheck){
+    console.log("Triple Equality check Successful")
+}
+// Testing === Check successful.
+
+
