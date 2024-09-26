8b2be494933473b4805bd90b9c4879361c52399d
Udit-D2002 <dudit3128@gmail.com>
2024-09-26 19:37:09 +0530

    Fixed long line
    Fixed long line of if/else assignment discription

--- Week-1-Websites/Week1Assignment/AssignmentJS.js
+++ Week-1-Websites/Week1Assignment/AssignmentJS.js
@@ -50,6 +50,7 @@ console.log(canVote(19));
 // Assignment #2 completed.
 
 
-// Assignment if/else (Write an if/else statement that checks if a number is even or odd. If it's even, print "The number is even." Otherwise, print "The number is odd.)
+// Assignment if/else (Write an if/else statement that checks if a number is even or odd. If it's even,
+// print "The number is even." Otherwise, print "The number is odd.)
 
 
