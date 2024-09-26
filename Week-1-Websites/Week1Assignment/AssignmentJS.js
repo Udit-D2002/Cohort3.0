f55e5245e8b4397dea149fc6705141442ead2fa6
Udit-D2002 <dudit3128@gmail.com>
2024-09-26 23:33:36 +0530

    Finished Basic Assignment Before Sleep

--- Week-1-Websites/Week1Assignment/AssignmentJS.js
+++ Week-1-Websites/Week1Assignment/AssignmentJS.js
@@ -51,6 +51,37 @@ console.log(canVote(19));
 
 
 // Assignment if/else (Write an if/else statement that checks if a number is even or odd. If it's even,
-// print "The number is even." Otherwise, print "The number is odd.)
+//                      print "The number is even." Otherwise, print "The number is odd.)
+
+function even_odd_teller(number){
+    if ((number % 2) === 0 ) {
+        return "The number is even."
+    } else{
+        return "The number is odd."
+    }
+} 
+console.log(even_odd_teller(0));
+// Assignment if/else solved using Function and if/else conditions.
+// Why I used function instead of simply using if/else statements :
+// because, how to write code of if/else statement without taking input from user or inputting the value of number?
+// I felt using function to solve the assignment would be a better approach.
+// Recently I figured out getting a user input in JS is a tricky part so i praise myself to use function :)
+
+
+// Assignment loops (Write a function called sum that finds the sum from 1 to a number)
+function sum(number){
+    let sum_till_now = 0;
+    for (let i = 0; i <= number; i++){
+        sum_till_now = sum_till_now + i
+    } return sum_till_now
+}
+console.log(sum(5));
+// Assignment loops completed.
+
+
+
+
+
+
 
 
